962aeb3974d9ce46ad4f2ca42577242a33634887
Miroslav Zorko <miroslav.zorko@sk.ibm.com>
2021-01-24 14:51:12 +0100

    Modified functional tests to check for multiple to do items. home.html accept new to-do item as variable tests.py modified to save a POST request view.py modiefied to accept POST and GET. POST display to-do items.

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.http import HttpRequest
 
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
